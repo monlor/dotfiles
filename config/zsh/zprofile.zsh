63147e018ad7af2468078abf9a3f76e059692881
monlor <me@monlor.com>
2023-08-29 14:51:21 +0800

    feat: 🎸 添加npmrc

--- config/zsh/zprofile.zsh
+++ config/zsh/zprofile.zsh
@@ -1,2 +1,9 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
-export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+if [ -d "/usr/local/Homebrew" ]; then
+  BREW_HOME=/usr/local/Homebrew
+elif [ -d "/opt/homebrew" ]; then
+  BREW_HOME=/opt/homebrew
+fi
+if [ -n "${BREW_HOME}" ]; then
+  eval "$(${BREW_HOME}/bin/brew shellenv)"
+  export HOMEBREW_BREW_GIT_REMOTE="https://mirrors.ustc.edu.cn/brew.git"
+fi
