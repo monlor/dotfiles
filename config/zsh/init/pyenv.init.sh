f787cfa0078130145e9f9a298a6804d758389ee4
monlor <me@monlor.com>
2024-05-18 20:11:34 +0800

    feat: 🎸 支持pyenv

--- config/zsh/init/pyenv.init.sh
+++ config/zsh/init/pyenv.init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 if type pyenv >/dev/null 2>&1; then
+  export PYENV_ROOT="$HOME/.pyenv"
+  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 fi
