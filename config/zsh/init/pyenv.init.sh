1dfd9d03f3842bca7116fb6e034c1b54c9aa35bb
monlor <me@monlor.com>
2024-06-29 22:38:14 +0800

    feat: 🎸 完善python的支持

--- config/zsh/init/pyenv.init.sh
+++ config/zsh/init/pyenv.init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export PYENV_ROOT="$HOME/.pyenv"
+[[ -d $PYENV_ROOT ]] && export PATH="$PYENV_ROOT/bin:$PATH"
+
 if type pyenv >/dev/null 2>&1; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
   eval "$(pyenv init -)"
 fi
